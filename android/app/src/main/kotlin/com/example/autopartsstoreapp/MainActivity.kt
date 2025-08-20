0e4535417e86e0b97d3dd293ea433373b0885ab3
Long <phandinhlong0209@gmail.com>
2025-08-21 00:07:35 +0700

    fix build android

--- android/app/src/main/kotlin/com/example/autopartsstoreapp/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.autopartsstoreapp
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
---
+++ android/app/src/main/kotlin/vn/callparts/MainActivity.kt
@@ -0,0 +1,5 @@
+package vn.callparts
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
