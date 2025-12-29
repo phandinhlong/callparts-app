195b97acf01abbc51e9249cb25738dbb41fb32be
NguyenMinhvn <23130193@st.hcmuaf.edu.vn>
2025-12-29 15:42:12 +0700

    khi giỏ hàng được thêm sản phẩm vào hoặc yêu thích sản phẩm không có xóa khi tắt app

--- lib/my_app.dart
+++ lib/my_app.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:callparts/presentation/providers/cart_provider.dart';
+import 'package:callparts/presentation/providers/favorite_provider.dart';
 
 import 'core/constants/app_colors.dart';
 import 'presentation/pages/splashScreen/splash_screen.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(const MyApp());
 }
 
@@ -13,14 +17,32 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Auto Parts App',
-      theme: ThemeData(
-        scaffoldBackgroundColor: AppColors.bgColor,
-        useMaterial3: true,
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (context) {
+            final cartProvider = CartProvider();
+            cartProvider.loadFromPreferences();
+            return cartProvider;
+          },
+        ),
+        ChangeNotifierProvider(
+          create: (context) {
+            final favoriteProvider = FavoriteProvider();
+            favoriteProvider.loadFromPreferences();
+            return favoriteProvider;
+          },
+        ),
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Auto Parts App',
+        theme: ThemeData(
+          scaffoldBackgroundColor: AppColors.bgColor,
+          useMaterial3: true,
+        ),
+        home: const SplashScreen(),
       ),
-      home: const SplashScreen(),
     );
   }
 }
